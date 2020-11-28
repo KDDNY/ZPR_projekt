3f6461842891f890df90566a570a65580970686c
Marcin <mpiotrowski1488@gmail.com>
2020-11-28 15:22:17 +0100

    male fixy po mergu, wszystko elegancko scalone jedynie metody do gui nie podpiete

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <wx/wx.h>
 #include <gtest/gtest.h>
-
-#include "classes/LocalDirectory.h"
-#include "classes/ScannerVisitor.h"
 #include "GUI classes/StartFrame.h"
+#include "classes/Application.h"
 
 /// Inicjuje bibliotekę graficzną
 class MainpApp : public wxApp{
     /// Wywyoływana w momencie uruchomienia aplikacji. Wyświetla startowe okno programu.
     bool OnInit(){
+
+        //Próba wyświetlenia w konsoli podstawowych metod
+        Profile* prof1 = new Profile(nullptr, nullptr,"___TEST PROFILE___");
+        prof1->addDirectories("/Users/raftry/Desktop/[ZPR]/testFolder1", "/Users/raftry/Desktop/[ZPR]/testFolder2",1,2);
+        prof1->summary();
+
+        delete prof1;
+
         wxPoint pos;
         start_frame_ = new StartFrame(pos);
         start_frame_->Show(true);
