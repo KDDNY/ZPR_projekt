b7325f232dfbba7c564b01c73ee8c0bdf8505ab6
Marcin <mpiotrowski1488@gmail.com>
2020-12-09 20:04:58 +0100

    Zrobiono porządek w Dir.h

--- classes/Dir.h
+++ classes/Dir.h
@@ -21,14 +21,28 @@ public:
     Dir(std::string path) : path_(path) {}
     virtual ~Dir() = default;
     virtual void printInfo() = 0;
-    virtual void assignPath(string path) = 0;
-    virtual void assignFactory(std::shared_ptr<AbstractFactory>) = 0;
-    virtual void addFile() = 0;
-    virtual void removeFile() = 0;
-    virtual void renameFile() = 0;
+    void assignPath(string path){
+        path_ = path;
+
+    }
+    void assignFactory(std::shared_ptr<AbstractFactory> factory) {
+        creator_ = factory;
+    }
+    void addFile(){
+        std::shared_ptr<Command> command = creator_->createAdd();
+        command->execute();
+    }
+    void removeFile(){
+        std::shared_ptr<Command> command = creator_->createRemove();
+        command->execute();
+    }
+    void renameFile(){
+        std::shared_ptr<Command> command = creator_->createRename();
+        command->execute();
+    }
     static Dir *make_dir(Choice flag);
     virtual void printTree() = 0;
-private:
+protected:
     string path_;
     std::shared_ptr<AbstractFactory> creator_;
 };
@@ -38,36 +52,14 @@ public:
     void printInfo(){
         std::cout << "#LOCAL DIR PATH: " << path_ <<std::endl;
     }
-    void assignPath(string path){
-        path_ = path;
-
-    }
     void printTree(){
 
         std::filesystem::path pathToShow(path_);
         DisplayDirTree(pathToShow,0);
 
     }
-    void assignFactory(std::shared_ptr<AbstractFactory> factory) {
-        creator_ = factory;
-    }
-    void addFile(){
-        std::shared_ptr<Command> command = creator_->createAdd();
-        command->execute();
-    }
-    void removeFile(){
-        std::shared_ptr<Command> command = creator_->createRemove();
-        command->execute();
-    }
-    void renameFile(){
-        std::shared_ptr<Command> command = creator_->createRename();
-        command->execute();
-    }
     void DisplayFileInfo(const filesystem::directory_entry & entry, string & lead, filesystem::path& file_name);
     void DisplayDirTree(const filesystem::path& pathToShow, int level);
-private:
-    string path_;
-    std::shared_ptr<AbstractFactory> creator_;
 };
 
 
@@ -76,33 +68,11 @@ public:
     void printInfo(){
         std::cout << "#SSH DIR PATH: " << path_ <<std::endl;
     }
-    void assignPath(string path){
-
-        path_ = path;
-    }
     void printTree(){
 
         cout << "--> WILL PRINT SSH DIR TREE" << endl;
 
     }
-    void assignFactory(std::shared_ptr<AbstractFactory> factory) {
-        creator_ = factory;
-    }
-    void addFile(){
-        std::shared_ptr<Command> command = creator_->createAdd();
-        command->execute();
-    }
-    void removeFile(){
-        std::shared_ptr<Command> command = creator_->createRemove();
-        command->execute();
-    }
-    void renameFile(){
-        std::shared_ptr<Command> command = creator_->createRename();
-        command->execute();
-    }
-private:
-    string path_;
-    std::shared_ptr<AbstractFactory> creator_;
 };
 
 
